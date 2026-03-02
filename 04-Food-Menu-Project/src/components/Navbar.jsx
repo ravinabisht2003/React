d623a82bd808a48d1419f42692dc2b8977da2945
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-03-02 21:11:00 +0530

    feat - add navbar

--- 04-Food-Menu-Project/src/components/Navbar.jsx
+++ 04-Food-Menu-Project/src/components/Navbar.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 
-import BottomBgImg from '../assets/BottomBgImg.png'
+import Logo from '../assets/Logo.png'
 
 function Navbar() {
   return (
     <div className='Navbar'>
-        
-      
-    </div>
+      <div className='Navbar-top'>
+      <img src={Logo} alt="" />
+      </div>
+      <div className='Navbar-nav'>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#order">Order</a></li>
+          <li><a href="#book">Book</a></li>
+          <li><a href="#catering">Catering</a></li>
+          <li><a href="#login">Login</a></li>
+          <li><a href="#cart">Cart</a></li>
+        </ul>
+
+      </div>
+      </div>
   )
 }
 
--- 04-Food-Menu-Project/src/components/OrderMenu.jsx
+++ 04-Food-Menu-Project/src/components/OrderMenu.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function OrderMenu() {
   return (
     <div>
-      
+      <h1>Hello</h1>
     </div>
   )
 }
