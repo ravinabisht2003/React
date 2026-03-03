777c16830ed6a09178e8fde3c5ff8030fe9e2a8b
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-03-03 20:41:37 +0530

    feat - adding features

--- 04-Food-Menu-Project/src/App.jsx
+++ 04-Food-Menu-Project/src/App.jsx
@@ -1,33 +1,68 @@
-import './App.css'
+import "./App.css";
 
-import OrderMenu from './components/OrderMenu'
-import Navbar from './components/Navbar'
-import Service from './components/Service'
-
+import OrderMenu from "./components/OrderMenu";
+import Navbar from "./components/Navbar";
+import Service from "./components/Service";
+import { Truck, Utensils, Armchair } from "lucide-react";
 
 function App() {
-
-
-
+  const services = [
+    {
+      icon: <Truck size={100}/>,
+      title: "Fast Delivery",
+      description: "You can fast order with delivery service, wait for minute then your food will come",
+    },
+    {
+      icon: <Utensils size={100} />,
+      title: "Catering",
+      description: "Order some menu with package for event like wedding, gathering meeting, and other else",
+    },
+    {
+      icon: <Armchair size={100}/>,
+      title: "Book a Table",
+      description: "Book a table for your family set number of person & arrange the event enjoy without worrying overcrowded",
+    },
+  ];
 
   return (
-  <div className='Order-Menu'>
-    <div className='Nav-Menu'>
-      <Navbar/> 
-    </div>
+    <div className="Order-Menu">
+      <div className="Nav-Menu">
+        <Navbar />
+      </div>
 
-    <div className='Service-Menu'>
-      <h1>Our Features</h1>
-      <h4>Let's check our service below <br/>you will get best service</h4>
-    
-      
-    </div>
-   <div className='Menu'>
-    <OrderMenu/>
+      <div className="Service-Menu">
+        <h1>Our Features</h1>
+        <h4>
+          Let's check our service below <br />
+          you will get best service
+        </h4>
 
-   </div>
-   </div>
-  )
+        
+        <div className="Service-Container">
+
+        {services.map(function(elem, indx){
+
+          return (
+
+          <div key={indx}>
+          <Service
+            icon={elem.icon}
+            title={elem.title}
+            description={elem.description}
+          />
+          </div>
+        );
+        })
+        }
+        </div>
+        
+
+      </div>
+      <div className="Menu">
+        <OrderMenu />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- 04-Food-Menu-Project/src/components/Service.jsx
+++ 04-Food-Menu-Project/src/components/Service.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function Service() {
+function Service(props) {
   return (
-    <div>
-      Hello
+    <div className='Service'>
+      <div className='Service-icons'>{props.icon }</div>
+      <div className='Service-title'>{props.title}</div>
+      <div className='Service-descr'>{props.description}</div>
+      <button>Check Out</button>
     </div>
   )
 }
