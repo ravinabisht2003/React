76f1dc5480cedae4199f0eadcc06b61e372890a5
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-03-03 09:59:17 +0530

    feat - add navbar

--- 04-Food-Menu-Project/src/components/Navbar.jsx
+++ 04-Food-Menu-Project/src/components/Navbar.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React from "react";
 
-import Logo from '../assets/Logo.png'
+import Logo from "../assets/Logo.png";
 
 function Navbar() {
   return (
-    <div className='Navbar'>
-      <div className='Navbar-top'>
-      <img src={Logo} alt="" />
+    <div className="Nav-Deails">
+      
+    <div className="Navbar">
+      <div className="Navbar-top">
+        <img src={Logo} alt="" />
       </div>
-      <div className='Navbar-nav'>
+      <div className="Navbar-nav">
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#order">Order</a></li>
-          <li><a href="#book">Book</a></li>
-          <li><a href="#catering">Catering</a></li>
-          <li><a href="#login">Login</a></li>
-          <li><a href="#cart">Cart</a></li>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#order">Order</a>
+          </li>
+          <li>
+            <a href="#book">Book</a>
+          </li>
+          <li>
+            <a href="#catering">Catering</a>
+          </li>
+          <li>
+            <a href="#login">Login</a>
+          </li>
+          <li>
+            <a href="#cart">Cart</a>
+          </li>
         </ul>
+      </div>
+    </div>
 
-      </div>
-      </div>
-  )
+    <div className="Navbar-Other">
+      <h1>Find Your Food</h1>
+      <h1>Delivered Fresh & hot</h1>
+    </div>
+
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
