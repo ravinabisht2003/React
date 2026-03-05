1000b2826c8726b326018273625157e3c4f0e676
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-03-05 20:26:32 +0530

    feat - add module css

---
+++ 05-Module-CSS/src/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Button.module.css';
+
+function Button() {
+  return (
+    <button className={styles.btn}>
+      Hello
+    </button>
+  )
+}
+
+export default Button
