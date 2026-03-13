47c49bdc47dbedfcbf9e7efc818521d863164807
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-03-13 07:41:39 +0530

    feat - added funtion onwheel

--- 08-Functions/src/App.jsx
+++ 08-Functions/src/App.jsx
@@ -3,13 +3,16 @@ import React from 'react'
 const App = () => {
   const fumctVar = function (elem){
     console.log(elem);
-    
   }
   return (
-    <div>
-      <button onClick={(elem)=>{
+    <div onWheel={fumctVar}>
+      {/* <button onClick={(elem)=>{
         fumctVar(elem.screenY)
-      } }>Click Here</button>
+      } }>Click Here</button> */}
+
+      <div className='box1'></div>
+      <div className='box2'></div>
+      <div className='box3'></div>
     </div>
   )
 }
