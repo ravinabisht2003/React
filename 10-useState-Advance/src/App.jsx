c79b9b4400f8bc7ac38c178dac7f0b33ee6c5824
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-03-26 08:41:05 +0530

    feat - array value change, object value change and batch update with the help of useState

--- 10-useState-Advance/src/App.jsx
+++ 10-useState-Advance/src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [num, setnum] = useState(20)
+  // const [num, setnum] = useState({user:"Ravina", age:22})
+  // const [num, setnum] = useState([10,20,30,40])
+
+
+    // object value change with the help of useState
+
+  //  const newNum = {...num};
+  //  newNum.user='Abhinav';
+  //  newNum.age=24;
+  //  setnum(newNum);
+
+  // array value change with the help of useState
+
+  // const newArray = [...num];
+  // newArray.push(90);
+  // setnum(newArray)
+
+  //second method of doing previous method
+
+  // setnum(prev=>({...prev,age:20}))
+
+  //batch update  method
+  const valueChngButtn = ()=>{
+    setnum(prev=>(prev+1));
+    setnum(prev=>(prev+1));
+    setnum(prev=>(prev+1));
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='main'>
+      
+        <h1>{num}</h1>
+        {/* <h1>{num.user}, {num.age}</h1> */}
+        {/* <h1>{num}</h1> */}
+
+        <button onClick={valueChngButtn}>Click</button>
+      
+    </div>
   )
 }
 
