65ae784670294c796e56191a4998853181f3e353
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-03-27 08:43:09 +0530

    feat - using two way bindding

--- 12-two-Way-Binding/src/App.jsx
+++ 12-two-Way-Binding/src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+
+  const [title, setTitle] = useState('')
  
   const formHandler = (e)=>{
     e.preventDefault()
     //It help to not reload the form.
-    console.log("Form Handler");
+    console.log("Form Handler by ", title);
+
+    setTitle('')
     
   }
 
@@ -14,7 +18,13 @@ function App() {
       <form onSubmit={(e)=>{
         formHandler(e)}
       }>
-        <input type="text" placeholder='Enter your Name' />
+        <input type="text"
+         placeholder='Enter your Name'
+         value={title}
+         onChange={(e)=>{
+          setTitle(e.target.value)
+         }}
+         />
         <button>Submit</button>
       </form>
     </div>
