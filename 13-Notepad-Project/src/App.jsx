7455c930aeacc2d0c09eee0d9edfd76f7ee70a1b
ravinabisht2003 <ravinabisht2003@gmail.com>
2026-04-04 07:54:15 +0530

    feat - added two way Binding

--- 13-Notepad-Project/src/App.jsx
+++ 13-Notepad-Project/src/App.jsx
@@ -4,9 +4,14 @@ import React, { useState } from 'react'
 function App() {
 
   const [title, setTitle] = useState("")
+  const [details, setDetails] = useState("")
 
   const formHandler = (e)=>{
     e.preventDefault()
+    console.log(title, details);
+    setTitle("")
+    setDetails("")
+    
   }
 
   return (
@@ -14,15 +19,30 @@ function App() {
 
       <form onSubmit={(e)=>{
         formHandler(e)
+        console.log("form submited")
       }}
       className='flex gap-4 lg:w-1/2 p-10 flex-col items-start' >
         <h1 className='text-3xl font-bold'>Add Notes</h1>
+        
         <input type="text" 
         placeholder='Enter Notes Heading'  
-        className='px-5 w-full font-medium py-2 border-2 outline-none rounded'/>
+        className='px-5 w-full font-medium py-2 border-2 outline-none rounded'
+        value={title}
+        onChange={(e)=>{
+          setTitle(e.target.value);
+        }}
+        />
+        
         <textarea name="text" 
         placeholder='Write Details'
-        className='px-5 w-full font-medium py-2 border-2 outline-none rounded'></textarea>
+        className='px-5 w-full font-medium py-2 border-2 outline-none rounded' 
+        value={details}
+        onChange={(e)=>{
+          setDetails(e.target.value)
+        }}
+        
+        ></textarea>
+
         <button className= 'bg-white font-medium w-full text-black outline-none px-5 py-2 rounded'>Add Note</button>
       </form>
       <div className='lg:w-1/2 lg:border-l-2 p-10'>
